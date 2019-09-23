9c3102567031ac9c21562ddc22b887ae714350c1
Administrator <2464867025@qq.com>
2019-09-23 09:06:08 +0800

    用户部门用户角色

--- src/main/java/cc/mrbird/febs/FebsApplication.java
+++ src/main/java/cc/mrbird/febs/FebsApplication.java
@@ -1,43 +1,56 @@
 package cc.mrbird.febs;
 
-import cc.mrbird.febs.common.interceptor.TokenInterceptor;
 import com.taobao.api.ApiException;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.util.Calendar;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * @author MrBird
  */
-@EnableScheduling
+//@EnableScheduling
+
 @SpringBootApplication
 @EnableAsync
 @EnableTransactionManagement
 @MapperScan("cc.mrbird.febs.*.mapper")
 @Configuration
-public class FebsApplication extends WebMvcConfigurerAdapter {
+public class FebsApplication {
 
     public static void main(String[] args) throws ApiException {
         new SpringApplicationBuilder(FebsApplication.class)
                 .run(args);
     }
 
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        //corsConfiguration.addExposedHeader("*");
+        return corsConfiguration;
+    }
+
     /**
-     * 配置拦截器
+     * 跨域过滤器
+     *
+     * @return
      */
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/**/**");
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
     }
 
 }
